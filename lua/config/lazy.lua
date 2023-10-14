6ea7ee7c0157528ccc5356dffe52a4ae0ee954e3
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2023-10-14 20:42:59 +0300

    Add fold ufo

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.coding.yanky' },
     { import = 'lazyvim.plugins.extras.dap.core' },
     { import = 'lazyvim.plugins.extras.dap.nlua' },
+    { import = 'lazyvim.plugins.extras.lsp.none-ls' },
     -- { import = "lazyvim.plugins.extras.formatting.prettierd" },
 
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,7 +1,7 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  'nvimtools/none-ls.nvim',
   opts = function(_, opts)
-    local nls = require("null-ls")
+    local nls = require('null-ls')
     table.insert(opts.sources, nls.builtins.formatting.prettier)
   end,
 }
