269d626042d7aaf9232e2f470d04d19f66bb880c
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2024-05-10 21:39:14 +0300

    Add endwise. Set terminal as windows

--- lua/plugins/terminal_conf.lua
+++ lua/plugins/terminal_conf.lua
@@ -1,5 +1,9 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = true,
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+    direction = 'horizontal',
+    close_on_exit = true,
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = { 'RRethy/nvim-treesitter-endwise' },
     opts = function(_, opts)
+      opts.endwise = { enable = true }
       vim.list_extend(opts.ensure_installed, {
         'ruby',
         'ninja',
