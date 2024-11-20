c476ba58bd6b438d7531af473fd140f9a98d417e
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2024-11-20 09:52:56 +0300

    Add disable autocomplete UI

--- lua/plugins/cmp_autocomplete.lua
+++ lua/plugins/cmp_autocomplete.lua
@@ -6,11 +6,29 @@ return {
     opts = function(_, opts)
       local cmp = require('cmp')
       opts.sources = {
-        { name = 'buffer' },
-        { name = 'nvim_lsp' }, -- Разобрался, работает нормально. Последовательность определяет порядок выдачи
         { name = 'luasnip' },
+        { name = 'nvim_lsp' }, -- Разобрался, работает нормально. Последовательность определяет порядок выдачи
+        { name = 'buffer' },
       }
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = 'emoji' } }))
     end,
+    keys = {
+      {
+        '<leader>ua',
+        function()
+          vim.g.cmp_disabled = not vim.g.cmp_disabled
+          local msg = ''
+          if vim.g.cmp_disabled == true then
+            msg = 'Autocompletion (cmp) disabled'
+          else
+            msg = 'Autocompletion (cmp) enabled'
+          end
+          vim.notify(msg, vim.log.levels.INFO)
+        end,
+        noremap = true,
+        silent = true,
+        desc = 'toggle autocompletion',
+      },
+    },
   },
 }
