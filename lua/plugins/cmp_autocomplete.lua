7897f68490f05378fe291442d7395834fd26c3be
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2024-11-20 11:05:09 +0300

    Add erb formatter (#5)
    * Add htmlbeautifier as erb formatter
    
    * Update packages
    
    * Update README
    
    * Add disable autocomplete UI
    
    * Disable autocomplete popup windows

--- lua/plugins/cmp_autocomplete.lua
+++ lua/plugins/cmp_autocomplete.lua
@@ -5,12 +5,40 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require('cmp')
+      opts.completion = {
+        autocomplete = false, -- Disable automatic popup
+      }
+      opts.mapping = {
+        ['<C-Space>'] = cmp.mapping.complete(), -- Trigger completion manually with Ctrl+Space
+        -- Other mappings...
+        ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection with Enter
+      }
       opts.sources = {
-        { name = 'buffer' },
-        { name = 'nvim_lsp' }, -- Разобрался, работает нормально. Последовательность определяет порядок выдачи
         { name = 'luasnip' },
+        { name = 'nvim_lsp' }, -- Разобрался, работает нормально. Последовательность определяет порядок выдачи
+        { name = 'buffer' },
       }
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = 'emoji' } }))
     end,
+    keys = {
+      {
+        '<leader>ua',
+        function()
+          vim.g.cmp_disabled = not vim.g.cmp_disabled
+          local msg = ''
+          if vim.g.cmp_disabled == true then
+            msg = 'Autocompletion (cmp) disabled'
+          else
+            msg = 'Autocompletion (cmp) enabled'
+          end
+          vim.notify(msg, vim.log.levels.INFO)
+        end,
+        noremap = true,
+        silent = true,
+        desc = 'toggle autocompletion',
+      },
+    },
   },
 }
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  'stevearc/conform.nvim',
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      ruby = { formatter },
+      eruby = { 'htmlbeautifier' },
+    },
+  },
+}
