75bfe5efd2b971da2005f4c0d4cc3eeeca100f24
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2024-12-14 10:51:51 +0300

    Add args to htmlbeatifier linter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,8 +3,13 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      ruby = { formatter },
+      ruby = { 'rubocop' },
       eruby = { 'htmlbeautifier' },
+    },
+    formatters = {
+      htmlbeautifier = {
+        prepend_args = { '-b', 1 },
+      },
     },
   },
 }
