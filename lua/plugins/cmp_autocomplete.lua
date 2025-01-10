3e09d3e6a0d8a90c686b1b92ec704bd4bf6218be
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2025-01-10 22:10:50 +0300

    Improve vim-cmp config. Remove lot of TEXT

--- lua/plugins/cmp_autocomplete.lua
+++ lua/plugins/cmp_autocomplete.lua
@@ -6,7 +6,7 @@ return {
     opts = function(_, opts)
       local cmp = require('cmp')
       opts.completion = {
-        autocomplete = false, -- Disable automatic popup
+        -- autocomplete = false, -- Disable automatic popup
       }
       opts.mapping = {
         ['<C-Space>'] = cmp.mapping.complete(), -- Trigger completion manually with Ctrl+Space
@@ -15,10 +15,24 @@ return {
         ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection with Enter
       }
+      opts.snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      }
       opts.sources = {
-        { name = 'luasnip' },
         { name = 'nvim_lsp' }, -- Разобрался, работает нормально. Последовательность определяет порядок выдачи
+        { name = 'luasnip' },
         { name = 'buffer' },
+        option = {
+          get_bufnrs = function()
+            local bufs = {}
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              bufs[vim.api.nvim_win_get_buf(win)] = true
+            end
+            return vim.tbl_keys(bufs)
+          end,
+        },
       }
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = 'emoji' } }))
     end,
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,12 +1,13 @@
 return {
   'nvimtools/none-ls.nvim',
-  opts = function(_, opts)
+  opts = function(_, _)
     local null_ls = require('null-ls')
 
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.completion.spell,
+        -- Following completion gave a lot of TEXT word !!!
+        -- null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.markdownlint_cli2,
         null_ls.builtins.formatting.prettier,
         -- null_ls.builtins.diagnostics.markdownlint,
