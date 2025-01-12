949549c1d27ea21a46290598661f03cdb270c722
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2025-01-12 22:37:38 +0300

    Fix gitlab.nvim errors

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -1,8 +1,8 @@
 return {
   'harrisoncramer/gitlab.nvim',
   dependencies = {
-    'MunifTanjim/nui.nvim',
-    'nvim-lua/plenary.nvim',
+    { 'MunifTanjim/nui.nvim', commit = '9e3916e' },
+    { 'nvim-lua/plenary.nvim', commit = '2d9b06177a975543726ce5c73fca176cedbffe9d' },
     'sindrets/diffview.nvim',
     'stevearc/dressing.nvim', -- Recommended but not required. Better UI for pickers.
     'nvim-tree/nvim-web-devicons', -- Recommended but not required. Icons in discussion tree.
