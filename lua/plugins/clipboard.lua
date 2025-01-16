89d7e74eb500ab9f0290a80df303ebf795a65285
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2025-01-16 21:39:43 +0300

    Temp fix for gitlab. Change timeout for yank

--- lua/plugins/clipboard.lua
+++ lua/plugins/clipboard.lua
@@ -1,1 +1,10 @@
-return { 'ibhagwan/smartyank.nvim' }
+return {
+  'ibhagwan/smartyank.nvim',
+  opts = {
+    highlight = {
+      enabled = true, -- highlight yanked text
+      higroup = 'IncSearch', -- highlight group of yanked text
+      timeout = 100, -- timeout for clearing the highlight
+    },
+  },
+}
--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -1,8 +1,8 @@
 return {
-  'harrisoncramer/gitlab.nvim',
+  'sergey-arkhipov/gitlab.nvim',
   dependencies = {
-    { 'MunifTanjim/nui.nvim', commit = '9e3916e' },
-    { 'nvim-lua/plenary.nvim', commit = '2d9b06177a975543726ce5c73fca176cedbffe9d' },
+    { 'MunifTanjim/nui.nvim' },
+    { 'nvim-lua/plenary.nvim' },
     'sindrets/diffview.nvim',
     'stevearc/dressing.nvim', -- Recommended but not required. Better UI for pickers.
     'nvim-tree/nvim-web-devicons', -- Recommended but not required. Icons in discussion tree.
