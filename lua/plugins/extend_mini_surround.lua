1a8adde9fa542c7013d820b23e7473cd6f976647
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-01-27 15:09:46 +0300

    Remap surround keys

--- lua/plugins/extend_mini_surround.lua
+++ lua/plugins/extend_mini_surround.lua
@@ -1,16 +1,30 @@
--- Add surrond support
+-- Change surround hot keys
 return {
-  'echasnovski/mini.surround',
-  recommended = true,
-  opts = {
-    mappings = {
-      add = 'gsa', -- Add surrounding in Normal and Visual modes
-      delete = 'gsd', -- Delete surrounding
-      find = 'gsf', -- Find surrounding (to the right)
-      find_left = 'gsF', -- Find surrounding (to the left)
-      highlight = 'gsh', -- Highlight surrounding
-      replace = 'gsr', -- Replace surrounding
-      update_n_lines = 'gsn', -- Update `n_lines`
+  -- Add new mappinngs to surround
+  {
+    'echasnovski/mini.surround',
+    opts = {
+      mappings = {
+        add = ';;',
+        delete = ';d',
+        find = ';f',
+        find_left = ';F',
+        highlight = ';h',
+        replace = ';r',
+        update_n_lines = ';n',
+      },
+    },
+  },
+
+  -- Remove ; mappings from flash plugin
+  {
+    'folke/flash.nvim',
+    opts = {
+      modes = {
+        char = {
+          keys = { 'f', 'F', 't', 'T' },
+        },
+      },
     },
   },
 }
