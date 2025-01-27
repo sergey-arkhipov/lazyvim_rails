d143f7c962f4bc45d288cfd539b6921e5c940325
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-01-27 13:59:04 +0300

    Remove dashboard from extras

--- lua/plugins/extend_dashboard.lua
+++ lua/plugins/extend_dashboard.lua
@@ -3,7 +3,6 @@ return {
   opts = function(_, opts)
     table.insert(
       opts.dashboard.preset.keys,
-      -- { 7, { icon = 'S', key = 'S', desc = 'Select Session', action = require('persistence').select } },
       5,
       { icon = 'ጠ ', key = 'm', desc = 'Gitlab MR', action = ":lua require('gitlab').review()" }
     )
--- lua/plugins/guess-indent.lua
+++ lua/plugins/guess-indent.lua
@@ -1,4 +1,5 @@
 return {
+  -- Automatically detect the indentation style used in a buffer and updating the buffer options accordingly
   'nmac427/guess-indent.nvim',
   opts = {
     auto_cmd = true,
