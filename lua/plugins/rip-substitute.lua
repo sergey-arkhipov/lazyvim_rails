aac259349827b2e1ae92e5c8b6686e4efb795922
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-01-28 12:11:54 +0300

    Add rip-substitute

---
+++ lua/plugins/rip-substitute.lua
@@ -0,0 +1,11 @@
+return {
+  'chrisgrieser/nvim-rip-substitute',
+  keys = {
+    {
+      'g/',
+      function() require('rip-substitute').sub() end,
+      mode = { 'n', 'x' },
+      desc = 'Rip Substitute',
+    },
+  },
+}
