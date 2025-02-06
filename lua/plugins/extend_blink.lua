1986739f26ad0b05a8f91b417a22fe9f9269242a
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-02-06 23:37:51 +0300

    Add enter presets to snippets.Conf in blink

--- lua/plugins/extend_blink.lua
+++ lua/plugins/extend_blink.lua
@@ -5,8 +5,23 @@ return {
     -- snippets = { preset = 'luasnip' },
     -- snippets = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
     keymap = {
-      preset = 'super-tab',
-      ['<C-y>'] = { 'select_and_accept' },
+      preset = 'enter',
+      -- ['<C-y>'] = { 'select_and_accept' },
+      -- ['<Up>'] = { 'select_prev', 'fallback' },
+      -- ['<Down>'] = { 'select_next', 'fallback' },
+      -- ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      -- ['<Tab>'] = { 'select_next', 'fallback' },
+      -- ['<Enter>'] = {
+      --   function(cmp)
+      --     if cmp.snippet_active() then
+      --       return cmp.accept()
+      --     else
+      --       return cmp.select_and_accept()
+      --     end
+      --   end,
+      --   'snippet_forward',
+      --   'fallback',
+      -- },
     },
   },
 }
