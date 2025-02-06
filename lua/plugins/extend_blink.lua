200306134366c718076cf4100e631c5cdfbd3f12
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-02-06 22:02:08 +0300

    Remove LuaSnip

--- lua/plugins/extend_blink.lua
+++ lua/plugins/extend_blink.lua
@@ -1,7 +1,9 @@
 return {
   'saghen/blink.cmp',
   opts = {
-    snippets = { preset = 'luasnip' },
+    -- Snippets work best without any additionals
+    -- snippets = { preset = 'luasnip' },
+    -- snippets = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
     keymap = {
       preset = 'super-tab',
       ['<C-y>'] = { 'select_and_accept' },
