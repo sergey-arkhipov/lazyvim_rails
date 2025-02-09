c288e20a04f4f107c94b0c8d1c29a7cec617be4c
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-02-09 22:35:10 +0300

    Add dimmed on startup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,3 +23,12 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'eruby', 'html.erb' },
   callback = function() vim.keymap.set('n', 'gf', ':Railsgf<cr>', { buffer = true, noremap = true, silent = true }) end,
 })
+
+-- Enable dimmed on startup
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = '*',
+  callback = function()
+    -- Use vim.schedule to ensure the command runs after Neovim is fully initialized.
+    vim.schedule(function() require('snacks').dim.enable() end)
+  end,
+})
