25ad9f63f29dd695152d2991726575f394898d7c
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-11-11 20:04:25 +0300

    Update packages

--- lua/plugins/extend_dashboard.lua
+++ lua/plugins/extend_dashboard.lua
@@ -1,9 +1,11 @@
 return {
   'folke/snacks.nvim',
   opts = function(_, opts)
+    table.remove(opts.dashboard.preset.keys, 3)
+    table.remove(opts.dashboard.preset.keys, 4)
     table.insert(
       opts.dashboard.preset.keys,
-      5,
+      3,
       { icon = 'ጠ ', key = 'm', desc = 'Gitlab MR', action = ":lua require('gitlab').review()" }
     )
     table.insert(
@@ -11,9 +13,6 @@ return {
       8,
       { icon = 'S', key = 'S', desc = 'Select Session', action = require('persistence').select }
     )
-    opts.dashboard.preset.keys[3] =
-      { icon = ' ', key = 'a', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" }
-    table.remove(opts.dashboard.preset.keys, 4)
     opts.dashboard.sections = {
       { section = 'header' },
       { section = 'keys', gap = 1, padding = 2 },
